cebb38795a9d726eecbbf869e9a7b23d0ea8b6a2
wuhao <wuhao02@os.vcredit.com>
2017-06-27 16:15:36 +0800

    mark

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -5,6 +5,7 @@ public class Main {
         System.out.println("i am at 16 08");
         System.out.println("i am at 15 45");
         System.out.println("i am devvtest! repair it at 1557");
+        System.out.println("mark wu 1613");
 
     }
 }
