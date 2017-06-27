9ed47d001f438948e3588b66846ced9801173d95
wuhao <wuhao02@os.vcredit.com>
2017-06-27 16:09:24 +0800

    wu 1609

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -2,6 +2,8 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        System.out.println("i am at 16 08");
+        System.out.println("i am at 15 45");
         System.out.println("i am devvtest! repair it at 1557");
 
     }
