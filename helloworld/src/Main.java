2fb1a0827c75491b6353a7f6d2b6e7935ba52aa9
wuhao <wuhao02@os.vcredit.com>
2017-06-27 15:58:25 +0800

    repair dev/test

--- helloworld/src/Main.java
+++ helloworld/src/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("i am devvtest!");
+        System.out.println("i am devvtest! repair it at 1557");
 
     }
 }
